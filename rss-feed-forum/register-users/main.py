3ad557ab73bb753f7a17f8b8de17eb885f5ca53f
Nala Bibliander <nala.bibliander@gmail.com>
2024-08-29 20:49:26 +0200

    unsubscribe function

--- rss-feed-forum/register-users/main.py
+++ rss-feed-forum/register-users/main.py
@@ -1,5 +1,5 @@
 from telegram.ext import ApplicationBuilder, CommandHandler
-from telegramBot import all_posts, qualification, start
+from telegramBot import all_posts, qualification, start, unsubscribe
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token('7486129110:AAEken4HTskhf5h6YltKI2fVVqXhhiIl3Cs').build()
@@ -9,6 +9,8 @@ if __name__ == '__main__':
     all_handler = CommandHandler('all', all_posts)
     application.add_handler(all_handler)
     qualification_handler = CommandHandler('qualification', qualification)
-    application.add_handler(qualification_handler)
+    application.add_handler(all_handler)
+    unsubscribe_handler = CommandHandler('unsubscribe', unsubscribe)
+    application.add_handler(unsubscribe_handler)
 
     application.run_polling()
